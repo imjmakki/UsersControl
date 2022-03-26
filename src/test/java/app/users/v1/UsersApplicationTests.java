74476e7fcdcbf075909391ecd1a193b7cef160bb
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-26 11:28:36 +0300

    Starting Core System for the users management

---
+++ src/test/java/app/users/v1/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package app.users.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
