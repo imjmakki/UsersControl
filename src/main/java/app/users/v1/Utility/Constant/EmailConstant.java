496007f8bdeb432d5cc91e7bc1e545835cc85615
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-28 13:22:13 +0300

    updating the user entity

--- src/main/java/app/users/v1/Utility/Constant/EmailConstant.java
+++ src/main/java/app/users/v1/Utility/Constant/EmailConstant.java
@@ -2,11 +2,11 @@ package app.users.v1.Utility.Constant;
 
 public class EmailConstant {
     public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
-    public static final String USERNAME = "email@saha.app"; // putting email here
+    public static final String USERNAME = "email@email.app"; // putting email here
     public static final String PASSWORD = "Password"; // putting email's password here
-    public static final String FROM_EMAIL = "info@info@saha.app";
-    public static final String CC_EMAIL = "support@saha.app";
-    public static final String EMAIL_SUBJECT = "Saha, LLC - New Password";
+    public static final String FROM_EMAIL = "info@email.app";
+    public static final String CC_EMAIL = "support@email.app";
+    public static final String EMAIL_SUBJECT = "Users, LLC - New Password";
     public static final String GMAIL_SMTP_SERVER = "smtp.gmail.com";
     public static final String SMTP_HOST = "mail.smtp.host";
     public static final String SMTP_AUTH = "mail.smtp.auth";
