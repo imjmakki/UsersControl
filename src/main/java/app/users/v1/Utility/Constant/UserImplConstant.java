279f331f3d198b492d0b4b98fcb4b22eaf013320
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-28 13:24:34 +0300

    updating the user entity

---
+++ src/main/java/app/users/v1/Utility/Constant/UserImplConstant.java
@@ -0,0 +1,16 @@
+package app.users.v1.Utility.Constant;
+
+public class UserImplConstant {
+    public static final String USERNAME_ALREADY_EXISTS = "Phone number already exists";
+    public static final String EMAIL_ALREADY_EXISTS = "Email already exists";
+    public static final String NO_USER_FOUND_BY_USERNAME = "No user found with this phone number: ";
+    public static final String FOUND_USER_BY_USERNAME = "Returning found user with this phone number: ";
+    public static final String NO_USER_FOUND_BY_EMAIL = "No user found with this email: ";
+    public static final String USER_DELETED_SUCCESSFULLY = "User deleted successfully";
+    public static final String FOUND_CAPTAIN_BY_PHONE = "Returning found captain with this phone number: ";
+    public static final String NO_CAPTAIN_FOUND_BY_EMAIL = "No captain found with this email: ";
+    public static final String CAPTAIN_DELETED_SUCCESSFULLY = "Captain deleted successfully";
+    public static final String FOUND_PLAYER_BY_PHONE = "Returning found player with this phone number: ";
+    public static final String NO_PLAYER_FOUND_BY_EMAIL = "No player found with this email: ";
+    public static final String PLAYER_DELETED_SUCCESSFULLY = "Player deleted successfully";
+}
