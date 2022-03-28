e9b2e665d002672c2dea739a90329b8b3fa3741c
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-28 14:06:25 +0300

    updating the maven dependencies

--- src/main/java/app/users/v1/Utility/Implement/UserServiceImpl.java
+++ src/main/java/app/users/v1/Utility/Implement/UserServiceImpl.java
@@ -1,51 +1,202 @@
 package app.users.v1.Utility.Implement;
 
+import app.users.v1.DAO.UserDAO;
 import app.users.v1.Domain.User;
+import app.users.v1.Service.EmailService;
+import app.users.v1.Service.LoginAttemptService;
 import app.users.v1.Service.UserService;
+import app.users.v1.Utility.Config.UserPrincipal;
+import app.users.v1.Utility.Enum.Role;
 import app.users.v1.Utility.Exception.User.*;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
+import javax.transaction.Transactional;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
-    @Override
-    public List<User> getUsers() {
-        return null;
+import static app.users.v1.Utility.Constant.UserImplConstant.*;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+
+@Service
+@Transactional
+@Qualifier("userDetailsService")
+public class UserServiceImpl implements UserService, UserDetailsService {
+    private Logger LOGGER = LoggerFactory.getLogger(getClass());
+    private UserDAO userDAO;
+    private BCryptPasswordEncoder passwordEncoder;
+    private LoginAttemptService loginAttemptService;
+    private EmailService emailService;
+
+    @Autowired
+    public UserServiceImpl(UserDAO userDAO, BCryptPasswordEncoder passwordEncoder, LoginAttemptService loginAttemptService, EmailService emailService) {
+        this.userDAO = userDAO;
+        this.passwordEncoder = passwordEncoder;
+        this.loginAttemptService = loginAttemptService;
+        this.emailService = emailService;
     }
 
     @Override
-    public User findUserByUsername(String username) {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDAO.findUserByUsername(username);
+        if (user == null) {
+            LOGGER.error(NO_USER_FOUND_BY_USERNAME + username);
+            throw new UsernameNotFoundException(NO_USER_FOUND_BY_USERNAME + username);
+        }else {
+            validateLoginAttempt(user);
+            user.setLastLoginDateDisplay(user.getLastLoginDate());
+            user.setLastLoginDate(new Date());
+            userDAO.save(user);
+            UserPrincipal userPrincipal = new UserPrincipal(user);
+            LOGGER.info(FOUND_USER_BY_USERNAME + username);
+            return userPrincipal;
+        }
     }
 
     @Override
-    public User findUserByEmail(String email) {
-        return null;
+    public User register(String username, String email, String password) throws UserNotFoundException, UsernameExistException, EmailExistException {
+        User user = new User();
+        user.setUserId(generateUserId());
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(encodePassword(password));
+        user.setRole(Role.USER.name());
+        user.setAuthorities(Role.USER.getAuthorities());
+        user.setJoinDate(new Date());
+        user.setActive(true);
+        user.setNotLocked(true);
+        userDAO.save(user);
+        return user;
     }
 
     @Override
-    public User register(String username, String email, String password) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException {
-        return null;
+    public User addNewUser(String username, String email, String password, String role, boolean isNonLocked, boolean isActive) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException, MessagingException {
+        validateNewUsernameAndEmail(EMPTY, username, email);
+        User user = new User();
+        user.setUserId(generateUserId());
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(encodePassword(password));
+        user.setRole(getRoleEnumName(role).name());
+        user.setAuthorities(getRoleEnumName(role).getAuthorities());
+        user.setJoinDate(new Date());
+        user.setActive(true);
+        user.setNotLocked(true);
+        userDAO.save(user);
+        return user;
     }
 
     @Override
-    public User addNewUser(String username, String email, String password, String role, boolean isNotLocked, boolean isActive) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException, MessagingException {
-        return null;
-    }
-
-    @Override
-    public User updateUser(String currentUsername, String newUsername, String newEmail, String role, boolean isNotLocked, boolean isActive) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException {
-        return null;
+    public User updateUser(String currentUsername, String newUsername, String newEmail, String role, boolean isNonLocked, boolean isActive) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException {
+        User currentUser = validateNewUsernameAndEmail(currentUsername, newUsername, newEmail);
+        currentUser.setUsername(newUsername);
+        currentUser.setEmail(newEmail);
+        currentUser.setActive(isActive);
+        currentUser.setNotLocked(isNonLocked);
+        currentUser.setRole(getRoleEnumName(role).name());
+        currentUser.setAuthorities(getRoleEnumName(role).getAuthorities());
+        userDAO.save(currentUser);
+        return currentUser;
     }
 
     @Override
     public void deleteUser(String username) {
-
+        User user = userDAO.findUserByUsername(username);
+        userDAO.deleteById(user.getId());
     }
 
     @Override
-    public void resetPassword(String email) throws MessagingException, EmailNotFoundException {
+    public List<User> getUsers() {
+        return userDAO.findAll();
+    }
 
+    @Override
+    public User findUserByUsername(String username) {
+        return userDAO.findUserByUsername(username);
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return userDAO.findUserByEmail(email);
+    }
+
+    private Role getRoleEnumName(String role) {
+        return Role.valueOf(role.toUpperCase());
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    private String generatePassword() {
+        return RandomStringUtils.randomAlphanumeric(20);
+    }
+
+    private String generateUserId() {
+        return RandomStringUtils.randomAlphanumeric(30);
+    }
+
+    // this is for reset user password with email
+    @Override
+    public void resetPassword(String email) throws MessagingException, EmailNotFoundException {
+        User user = userDAO.findUserByEmail(email);
+        if (user == null) {
+            throw new EmailNotFoundException(NO_USER_FOUND_BY_EMAIL + email);
+        }
+        String password = generatePassword();
+        user.setPassword(encodePassword(password));
+        userDAO.save(user);
+        LOGGER.info("Your new user password is: " + password);
+        emailService.sendNewPasswordEmail(user.getUsername(), password, user.getEmail());
+    }
+
+    private void validateLoginAttempt(User user) {
+        if(user.isNotLocked()) {
+            if(loginAttemptService.hasExceededMaxAttempts(user.getUsername())) {
+                user.setNotLocked(false);
+            } else {
+                user.setNotLocked(true);
+            }
+        } else {
+            loginAttemptService.evictUserFromLoginAttemptCache(user.getUsername());
+        }
+    }
+
+    private User validateNewUsernameAndEmail(String currentUsername, String newUsername, String newEmail) throws UserNotFoundException, UsernameExistException, EmailExistException {
+        User userByNewUsername = findUserByUsername(newUsername);
+        User userByNewEmail = findUserByEmail(newEmail);
+        if(isNotBlank(currentUsername)) {
+            User currentUser = findUserByUsername(currentUsername);
+            if(currentUser == null) {
+                throw new UserNotFoundException(NO_USER_FOUND_BY_USERNAME + currentUsername);
+            }
+            if(userByNewUsername != null && !currentUser.getId().equals(userByNewUsername.getId())) {
+                throw new UsernameExistException(USERNAME_ALREADY_EXISTS);
+            }
+            if(userByNewEmail != null && !currentUser.getId().equals(userByNewEmail.getId())) {
+                throw new EmailExistException(EMAIL_ALREADY_EXISTS);
+            }
+            return currentUser;
+        } else {
+            if(userByNewUsername != null) {
+                throw new UsernameExistException(USERNAME_ALREADY_EXISTS);
+            }
+            if(userByNewEmail != null) {
+                throw new EmailExistException(EMAIL_ALREADY_EXISTS);
+            }
+            return null;
+        }
     }
 }
