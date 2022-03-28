a3e978dc016616db7a4a091a4af54ffabc0e2fed
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-28 13:24:00 +0300

    updating the user entity

---
+++ src/main/java/app/users/v1/Utility/Constant/FileConstant.java
@@ -0,0 +1,10 @@
+package app.users.v1.Utility.Constant;
+
+public class FileConstant {
+    public static final String DIRECTORY_CREATED = "Created directory for: ";
+    public static final String FILE_SAVED_IN_FILE_SYSTEM = "Saved file in file system by name: ";
+    public static final String DOT = ".";
+    public static final String FORWARD_SLASH = "/";
+    public static final String NOT_AN_IMAGE_FILE = " is not an image file. Please upload an image file";
+    public static final String TEMP_PROFILE_IMAGE_BASE_URL = "https://robohash.org/";
+}
