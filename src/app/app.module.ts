cc2ddab9ceef6fd6f1b9a5cdf5e4501419f969ea
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-29 14:21:50 +0300

    Users updating the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './layout/access/login/login.component';
+import { RegisterComponent } from './layout/access/register/register.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { MainComponent } from './layout/main/main.component';
+import { UsersComponent } from './layout/users/users.component';
+import { AddUserComponent } from './layout/add-user/add-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    MainComponent,
+    UsersComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule
