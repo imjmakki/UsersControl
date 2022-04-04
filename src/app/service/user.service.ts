baf21464a3e26bd8a9dca6f19907f523087b11a4
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-04 16:08:06 +0300

    adding jwt for access control

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -3,7 +3,6 @@ import {HttpClient, HttpErrorResponse, HttpResponse} from "@angular/common/http"
 import { environment } from "../../environments/environment";
 import {Observable} from "rxjs";
 import {User} from "../model/user";
-import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
