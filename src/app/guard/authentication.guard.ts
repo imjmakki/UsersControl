8ac43ce8ad7e410a05930cabfc3e7b83fd56a239
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-05 17:17:08 +0300

    adding Guard

--- src/app/guard/authentication.guard.ts
+++ src/app/guard/authentication.guard.ts
@@ -10,9 +10,7 @@ export class AuthenticationGuard implements CanActivate {
 
   constructor(private authenticationService: AuthenticationService, private router: Router) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return true;
   }
 
