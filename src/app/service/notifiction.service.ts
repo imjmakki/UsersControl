8296dc9811be3933ef700c8684284d8a6bc84c9b
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-06 18:17:34 +0300

    adding Notification Service

--- src/app/service/notifiction.service.ts
+++ src/app/service/notifiction.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { NotifierService } from 'angular-notifier';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class NotifictionService {
+@Injectable({providedIn: 'root'})
+export class NotificationService {
 
-  constructor() { }
+  constructor(private notifier: NotifierService) {}
+
+  public notify(type: NotificationType, message: string) {
+    this.notifier.notify(type, message);
+  }
 }
